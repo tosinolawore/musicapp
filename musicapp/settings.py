6413f928c18e60a43a65a43cca5e138ebb936f1b
Tosin Olawore <25176099+tosinolawore@users.noreply.github.com>
2022-04-09 20:34:53 +0100

    Updates README.md with CircleCI status badge and settings with db info

--- musicapp/settings.py
+++ musicapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'musicapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'musicapp',
+        'USER':'musicapp',
+        'PASSWORD':'musicapp',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
